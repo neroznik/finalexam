d0c7251daad1e5fa62ebc2aa747c4e972c05731d
Evgenii <neroznik@yandex.ru>
2021-01-23 13:36:42 +0600

    Созданы представления для модели Message, прописаны urls

--- source/webapp/views/message_view.py
+++ source/webapp/views/message_view.py
@@ -1,6 +1,64 @@
-# if self.request.user.is_authenticated:
-#     context = super().get_context_data(**kwargs)
-#     user = self.request.user
-#     myuser = MyUser.objects.filter(user=user)
-#     context['user'] = myuser
-#     return context
+from django.shortcuts import get_object_or_404
+from django.views.generic import ListView, View, TemplateView, CreateView
+from django.urls import reverse
+
+from webapp.forms import MessageForm
+from webapp.models import Message
+
+class InboxView(ListView):
+    template_name = 'inbox.html'
+    context_object_name = 'message'
+    paginate_by = 10
+    paginate_orphans = 0
+    model = Message
+    ordering = ['-created_at']
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        message = Message.objects.filter(reciever=self.request.user)
+        context['message'] = message
+        return context
+
+class SentView(ListView):
+    template_name = 'sent.html'
+    context_object_name = 'message'
+    paginate_by = 10
+    paginate_orphans = 0
+    model = Message
+    ordering = ['-created_at']
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        message = Message.objects.filter(sender = self.request.user)
+        context['message'] = message
+        return context
+
+
+class MessageTextView (TemplateView):
+    template_name = 'message.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        pk = self.kwargs.get('pk')
+        message = get_object_or_404(Message, pk=pk)
+        context['message'] = message
+        return context
+
+
+
+class MessageCreateView(CreateView):
+    template_name = 'message_add.html'
+    form_class = MessageForm
+    model = Message
+
+    def form_valid(self, form):
+        message = form.save(commit=False)
+        message.sender = self.request.user
+        message.save()
+        return super().form_valid(form)
+
+    def get_success_url(self):
+        return reverse('webapp:picture_view', kwargs={'pk': self.object.pk})
+
+
